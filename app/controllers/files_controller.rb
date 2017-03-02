46323f95eab979d27414ddb66c88256389e7d7b5
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-03-02 00:46:21 -0500

    CRUD | Show, Edit, New

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,34 @@
 class FilesController < ApplicationController
   def index
-    #@files = Dir["files/*"]
     @files = Dir.entries("#{Dir.pwd}/files/").select {|f| !File.directory? f}
   end
 
   def show
+    @file = params['name']
+    @data = File.read("#{Dir.pwd}/files/#{@file}")
+  end
+
+
+  def new
+    @file = ''
+
+  end
+
+  def create
+    File.open("files/#{params['name']}", "w+") do |f|
+      f.write(params['content'])
+    end
+    redirect_to file_path(name: params['name'])
+  end
+
+  def update
+    File.open("files/#{params['name']}", "w+") do |f|
+      f.write(params['content'])
+    end
+    redirect_to file_path(name: params['name'])
+  end
+
+  def edit
     @file = params['name']
     @data = File.read("#{Dir.pwd}/files/#{@file}")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'files', to: 'files#index'
-
+  get 'files', to: 'files#index' 
+  get 'files/new', to: 'files#new'
+  post 'files/new', to: 'files#create'
   get 'files/:name', to: 'files#show', as: 'file'
+  get 'files/:name/edit', to: 'files#edit', as: 'edit_file'
+  put 'files/:name', to: 'files#update', as: 'update_file'
 end
