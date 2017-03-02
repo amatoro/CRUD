f3a7dd1138a54b486a2b7706abb4443f658557a5
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-03-01 19:07:40 -0500

    CRUD Example

---
+++ app/controllers/files_controller.rb
@@ -0,0 +1,12 @@
+class FilesController < ApplicationController
+  def index
+    #@files = Dir["files/*"]
+    @files = Dir.entries("#{Dir.pwd}/files/").select {|f| !File.directory? f}
+  end
+
+  def show
+    @file = params['name']
+    @data = File.read("#{Dir.pwd}/files/#{@file}")
+  end
+
+end
